3ac46064863113de4b88bea98ddfa98d39630805
Nikita-div <motiki839@gmail.com>
2019-07-18 17:42:49 +0300

    Функция работает

--- formRegistration.js
+++ formRegistration.js
@@ -1,3 +1,18 @@
 function Autorization() {
-    
+    var autlogin='login';
+    var autpass='password';
+  localstorage['log']=autlogin;
+  localstorage['pass']=autpass;
+
+}
+
+function foolproof() {
+    var pass='password';
+    var checkPass='password_check';
+    if (pass===checkPass) {
+        Autorization();
+    } 
+    else {
+      alert('Пароли не совпадают')    
+    }
 }
